17f1eb74abe9b4ab25472d5e8b53d27abc5028a9
UUuser492 <74678882+UUuser492@users.noreply.github.com>
2025-03-04 17:53:42 +0200

    Lab1 DiscreteAlarms with BaseAlarm classes
    Базовий клас тривог із класом дискретних тривог

---
+++ OPCua/OPCua/DiscreteAlarm.cs
@@ -0,0 +1,16 @@
+﻿using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPCua
+{
+    class DiscreteAlarm : BaseAlarm
+    {
+        public bool ActiveStateId { get; set; }//стан аварії
+        public DateTime ActiveStateTransitionTime { get; set; }//час останньої зміни стану
+        public NodeId? NormalState { get; set; }//властивість, що вказує на змінну в нормальному стані
+    }
+}
---
+++ OPCua/OPCua/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Opc.Ua;
+
+Console.WriteLine("");
