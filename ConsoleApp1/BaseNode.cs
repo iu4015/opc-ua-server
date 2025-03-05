9ff2173d22136082b35124f959615e064fc3c913
Andriy <ukraineforce07@gmail.com>
2025-03-05 23:06:14 +0200

    +атрибути

--- ConsoleApp1/BaseNode.cs
+++ ConsoleApp1/BaseNode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleReferenceServer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -7,6 +8,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleReferenceServer
 {
+
+    public class BaseNode
+    {
+        public required NodeId NodeId { get; set; }
+        public required DisplayName DisplayName { get; set; }
+        public required string FullPath { get; set; }
+        public required NodeId DataType { get; set; }
+        public required int ValueRank { get; set; }
+        public required byte AccessLevel { get; set; }
+        public required List<UserRolePermission> UserRolePermissions { get; set; }
+
+
+    }
     public enum IdType
     {
         Numeric = 0,
@@ -29,69 +43,25 @@ namespace ConsoleReferenceServer
     }
     public struct NodeId
     {
-        public ushort NamespaceIndex;
-        public IdType IdentifierType;
-        public object Identifier;
-        public NodeId(ushort namespaceIndex, int identifierType, object identifier)
-        {
-            NamespaceIndex = namespaceIndex;
-            IdentifierType = (IdType)identifierType;
-            SetIdentifier(identifier);
-        }
+        public required ushort NamespaceIndex { get; set; }
+        public IdType IdentifierType { get; set; }
+        public required object Identifier { get; set; }
 
-        private void SetIdentifier(object identifier)
-        {
-            switch (IdentifierType)
-            {
-                case IdType.Numeric when identifier is int:
-                case IdType.String when identifier is string:
-                case IdType.Guid when identifier is Guid:
-                case IdType.Opaque: // Будь-який формат, визначений сервером
-                    Identifier = identifier;
-                    break;
-                default:
-                    throw new ArgumentException("Невірний тип ідентифікатора для обраного IdType.");
-            }
-        }
+   
+     }
 
-    }
-
-    public class BrowseName
-    {
-        public ushort NamespaceIndex { get; set; }
-        public string Name { get; set; }
-
-        public BrowseName(ushort namespaceIndex, string name)
-        {
-            NamespaceIndex = namespaceIndex;
-            Name = name;
-        }
-    }
 
     public class DisplayName
     {
-        public string Locale { get; set; }
-        public string Text { get; set; }
+        public required string Locale { get; set; }
+        public required string Text { get; set; }
 
-        public DisplayName(string locale, string text)
-        {
-            Locale = locale;
-            Text = text;
-        }
     }
-    public class BaseNode
+
+    public class UserRolePermission
     {
-        public NodeId nodeId;
-        public NodeClass nodeClass;
-        public BrowseName browseName;
-        public DisplayName displayName;
-
-        public BaseNode(NodeId nodeid, int nodeclass, BrowseName browsename, DisplayName displayname)
-        {
-            nodeId = nodeid;
-            nodeClass = (NodeClass)nodeclass;
-            browseName = browsename;
-            displayName = displayname;
-        }
+        public NodeId RoleId { get; set; }
+        public uint Permissions { get; set; }
     }
+  
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using ConsoleReferenceServer;
+using System.Text.Json;
 
-var node = new BaseNode(new NodeId(1, 0, 1), 2, new BrowseName(1, "TemperatureSensor"), 
-    new DisplayName("uk-UA", "Датчик температури"));
-Console.WriteLine("Hello, World!");
+List<UserRolePermission> roles = [
+    new UserRolePermission() {
+RoleId = new NodeId()
+{
+    NamespaceIndex = 1,
+    IdentifierType = IdType.Numeric,
+    Identifier = 2
+},
+Permissions = 10
+},
+new UserRolePermission() {
+RoleId = new NodeId()
+{
+    NamespaceIndex = 1,
+    IdentifierType = IdType.String,
+    Identifier = '2'
+},
+Permissions = 10
+},
+];
+
+BaseNode nodetest = new()
+{
+    NodeId = new NodeId()
+    {
+        NamespaceIndex = 4,
+        IdentifierType = IdType.String,
+        Identifier = "ssss"
+    },
+    DisplayName = new DisplayName
+    {
+        Locale = "UK",
+        Text = "TT"
+    },
+    FullPath = "Base/TT",
+    DataType = new NodeId()
+    {
+        NamespaceIndex = 5,
+        IdentifierType = IdType.Numeric,
+        Identifier = 222
+    },
+    ValueRank = 2,
+    AccessLevel = 0,
+    UserRolePermissions = [.. roles]
+
+};
+
+string jsonString = JsonSerializer.Serialize(nodetest);
+
+//var node = new BaseNode(new NodeId(1), new LocalizedText("UK", "TT"),
+//    "Base/TT", new NodeId(2),new Variant(true),2,0,new RolePermissionTypeCollection(2));
+Console.WriteLine(jsonString);
