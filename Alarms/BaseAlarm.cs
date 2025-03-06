6632ab27b55c0eb08d679d3317aea90b3ae3ebf4
UUuser492 <codexcepper@gmail.com>
2025-03-06 15:05:35 +0200

    Комміт в новій гілці для DiscreteAlarm

---
+++ Alarms/BaseAlarm.cs
@@ -0,0 +1,22 @@
+using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OPCua
+{
+    class BaseAlarm
+    {
+        public string? EventId { get; set; }//ідентифікатор тривоги
+        public NodeId? EventType { get; set; }//тип події (аналогова, чи дискретна)
+        public NodeId? SourceNode { get; set; }//вузол, що ініціював тривогу
+        public string? SourceName { get; set; }//текстове ім'я джерела події
+        public LocalizedText? Message { get; set; }//текстове повідомлення тривоги
+        public ushort Severity { get; set; }//рівень критичності тривоги
+        public int OnDelay { get; set; }//налаштування часу для стрибків сигналу, які не спричинять тривогу 
+        public int OffDelay { get; set; } //налаштування часу для падінь сигналу, які не спричинять тривогу
+    }
+}
---
+++ Alarms/DiscreteAlarm.cs
@@ -0,0 +1,14 @@
+using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPCua
+{
+    class DiscreteAlarm : BaseAlarm
+    {
+
+    }
+}
