4d59d7d0e63c18ae48b28dbe2bbe89ac046961a1
Andriy <ukraineforce07@gmail.com>
2025-04-09 20:03:21 +0300

    Config code update

--- Services/AddConfig.cs
+++ Services/AddConfig.cs
@@ -2,67 +2,76 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using Microsoft.Extensions.Options;
 
 
-public class AddConfig
+
+public class AddConfig : IAddConfig
 {
-    public List<BaseNode>? LoadBaseNodeConfig()
+    private readonly NodeConfigPaths _config;
+
+    public AddConfig(IOptions<NodeConfigPaths> config)
+    {
+        _config = config.Value;
+    }
+
+    public async Task<IEnumerable<BaseNode>> LoadBaseNodeConfig()
     {
         try
         {
-            if (!File.Exists("Filepath"))
+            if (!File.Exists(_config.BaseNodePath))
             {
                 Console.WriteLine("Конфігурація основних вузлів відсутня");
-                return null;
+                return Enumerable.Empty<BaseNode>();
             }
 
-            string json = File.ReadAllText("Filepath");
-            return JsonSerializer.Deserialize<List<BaseNode>>(json);
+            string json = await File.ReadAllTextAsync(_config.BaseNodePath);
+            var result = JsonSerializer.Deserialize<List<BaseNode>>(json);
+            if (result == null)
+            {
+                return Enumerable.Empty<BaseNode>();
+            }
+            return result.AsEnumerable();
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Помилка считування конфігу основних вузлів: {ex.Message}");
-            return null;
+            return Enumerable.Empty<BaseNode>();
         }
     }
 
-    public List<AnalogAlarm>? LoadAnalogAlarmNodeConfig()
+    public async Task<IEnumerable<BaseAlarm>> LoadAlarmNodeConfig()
     {
+        var result = new List<BaseAlarm>();
         try
         {
-            if (!File.Exists("Filepath"))
+            if (!File.Exists(_config.AnalogAlarmPath))
             {
                 Console.WriteLine("Конфігурація вузлів аналогових аварій відсутня");
-                return null;
+                return Enumerable.Empty<BaseAlarm>();
             }
 
-            string json = File.ReadAllText("Filepath");
-            return JsonSerializer.Deserialize<List<AnalogAlarm>>(json);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Помилка считування конфігу вузлів аналогових аварій: {ex.Message}");
-            return null;
-        }
-    }
+            string analogJSON = await File.ReadAllTextAsync(_config.AnalogAlarmPath);
+            var analogAlarms = JsonSerializer.Deserialize<List<AnalogAlarm>>(analogJSON);
+            if (analogAlarms != null)
+                result.AddRange(analogAlarms);
 
-    public List<DiscreteAlarm>? LoadAnalogAlarmNodeConfig()
-    {
-        try
-        {
-            if (!File.Exists("Filepath"))
+            if (!File.Exists(_config.DiscreteAlarmPath))
             {
                 Console.WriteLine("Конфігурація вузлів дискретних аварій відсутня");
-                return null;
+                return Enumerable.Empty<BaseAlarm>();
             }
 
-            string json = File.ReadAllText("Filepath");
-            return JsonSerializer.Deserialize<List<DiscreteAlarm>>(json);
+            string discreteJSON = await File.ReadAllTextAsync(_config.DiscreteAlarmPath);
+            var discreteAlarms = JsonSerializer.Deserialize<List<DiscreteAlarm>>(discreteJSON);
+            if (discreteAlarms != null)
+                result.AddRange(discreteAlarms);
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Помилка считування конфігу вузлів дискретних аварій: {ex.Message}");
-            return null;
+            Console.WriteLine($"Помилка считування конфігу вузлів аварій: {ex.Message}");
+            return Enumerable.Empty<BaseAlarm>();
         }
+        return result.AsEnumerable();
     }
 }
